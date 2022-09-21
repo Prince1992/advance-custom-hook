3173b553d7ea973f1d9f7b943a2a853be4e564cd
Amar Singh Yadav <amarprince9@gmail.comm>
2022-09-21 20:28:02 +0530

    Custom Hook

--- src/App.js
+++ src/App.js
@@ -6,27 +6,26 @@ import { useHttp } from './hooks/use-http';
 
 function App() {
   const [tasks, setTasks] = useState([]);
-  const transformedTasks = (taskObj) => {
-    const loadedTasks = [];
 
-    for (const taskKey in taskObj) {
-      loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
-    }
-
-    setTasks(loadedTasks);
-  };
-  const {
-    isLoading,
-    error,
-    sendRequest: fetchTasks,
-  } = useHttp(
-    { url: 'https://react-http-4431d-default-rtdb.firebaseio.com/tasks.json' },
-    transformedTasks
-  );
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
 
   useEffect(() => {
-    fetchTasks();
-  }, []);
+    const transformedTasks = (taskObj) => {
+      const loadedTasks = [];
+
+      for (const taskKey in taskObj) {
+        loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
+      }
+
+      setTasks(loadedTasks);
+    };
+    fetchTasks(
+      {
+        url: 'https://react-http-4431d-default-rtdb.firebaseio.com/tasks.json',
+      },
+      transformedTasks
+    );
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
